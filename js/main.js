802a2485f83cecb388cc820ef347e93828a8350c
TheSchrodingerCat <mariaj.pozom@gmail.com>
2017-04-27 11:22:13 -0300

    se cambia forEach por for para agregar las primeras 10 tareas y funciona. Tambien se le suman a la lista las tareas nuevas. Falta limpiar el input despues de cada ingreso

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
+
 var primeros10 = [];
+var list = document.getElementById("lista");
 for (i=0 ; i<10 ; i++){
 	primeros10.push(arregloTareas[i]);
+	list.innerHTML += "<li>" + primeros10[i].title + "</li>";
 }
 
-primeros10.forEach(function(element){document.write("<ul>" + "<li>" + element.title + "</li>" + "</ul>");});
+//lista10 = primeros10.forEach(function(element){"<ul>" + "<li>" + element.title + "</li>" + "</ul>";})
+
 
 function agregarTarea(){
 	var newWork = document.getElementById("tareaNueva");
@@ -18,5 +22,6 @@ function agregarTarea(){
 	var tareaExtra = new Tarea(1,primeros10.length+1,newWork.value,false); //porque se supone que son tareas pendientes
 	primeros10.push(tareaExtra);
 
-	
+	var list = document.getElementById("lista");
+	list.innerHTML += "<li>" + primeros10[primeros10.length-1].title + "</li>";
 }
