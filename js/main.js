c091d30b2c58d927b35fabba7a8725e0755cb029
TheSchrodingerCat <mariaj.pozom@gmail.com>
2017-04-27 10:26:01 -0300

    se crea la funcion agregarTarea(), dentro de la cual se crea el objeto Tarea, para agregarla a la lista de las 10 primeras tareas del JSON, con atributos y todo (no se si esta bien...)

--- js/main.js
+++ js/main.js
@@ -4,3 +4,19 @@ for (i=0 ; i<10 ; i++){
 }
 
 primeros10.forEach(function(element){document.write("<ul>" + "<li>" + element.title + "</li>" + "</ul>");});
+
+function agregarTarea(){
+	var newWork = document.getElementById("tareaNueva");
+
+	function Tarea(userId,id,title,completed){
+		this.userId = userId;
+		this.id = id;
+		this.title = title;
+		this.completed = completed;
+	}
+
+	var tareaExtra = new Tarea(1,primeros10.length+1,newWork.value,false); //porque se supone que son tareas pendientes
+	primeros10.push(tareaExtra);
+
+	
+}
